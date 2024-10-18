0ddba9216b54595c99be8566f6303a7d623a20ef
Raunak <raunakkumar1611@gmail.com>
2024-10-19 00:00:50 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -9,15 +9,37 @@ const userSchema = new mongoose.Schema({
   },
   emailId: {
     type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
   },
   age: {
     type: Number,
+    min: 18,
+    max: 150,
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender is not valid");
+      }
+    },
+  },
+  photoURL: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA6g9BWr61gs6KYIq3zjFEy36Z8OuOIJQ75A&s",
+  },
+  about: {
+    type: String,
+    default: "This is a default about the user!",
+  },
+  skills: {
+    type: [String],
   },
 });
 //model
